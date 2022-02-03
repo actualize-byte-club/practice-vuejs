54d63a09816063e618660f3d53cd3658ac16b60c
dani <dani.zaghian@gmail.com>
2022-02-03 12:14:36 -0800

    adds v-for

--- calculator.js
+++ calculator.js
@@ -4,16 +4,14 @@ var App = {
       message: "Hello Vue!",
       number1: 0,
       number2: 0,
-      number3: 0,
-      sum: 0
+      number3: 0
     };
   },
   methods: {
     sumNums: function () {
-      this.sum =
-        parseInt(this.number1) +
-        parseInt(this.number2) +
-        parseInt(this.number3);
+      return (
+        parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3)
+      );
     }
   }
 };
--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var App = {
   data: function () {
     return {
       message: "Hello from JavaScript!",
-      name: "Dani"
+      name: "Dani",
+      fruits: ["apple", "banana", "cantaloupe", "grapes", "figs"],
+      newFruit: ""
     };
   },
   methods: {
     changeMessage: function () {
       this.message = "Whoa, this is cool";
+    },
+    addNewFruit: function () {
+      this.fruits.push(this.newFruit);
+      this.newFruit = "";
     }
   }
 };
