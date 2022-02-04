cfffbba805b5e5f5c8bdf454fb2f74c145fe8af2
dani <dani.zaghian@gmail.com>
2022-02-04 11:20:26 -0800

    adds vue created option:

--- todos.js
+++ todos.js
@@ -7,15 +7,11 @@ var App = {
       todos: []
     };
   },
-  methods: {
-    loadTodos: function () {
-      axios
-        .get("https://jsonplaceholder.typicode.com/todos")
-        .then((response) => {
-          console.log(response.data);
-          this.todos = response.data;
-        });
-    }
+  created: function () {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+      console.log(response.data);
+      this.todos = response.data;
+    });
   }
 };
 
